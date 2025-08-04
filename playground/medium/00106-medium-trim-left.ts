16eaff37e9c162e89499cd5e495f1a4d6594e315
Gepoverlow <gepeoverload@gmail.com>
2025-08-04 17:30:56 +0200

    medium trim left

--- playground/medium/00106-medium-trim-left.ts
+++ playground/medium/00106-medium-trim-left.ts
@@ -18,7 +18,8 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TrimLeft<S extends string> = any
+type Whitespace = ' ' | '\n' | '\t'
+type TrimLeft<S extends string> = S extends `${Whitespace}${infer U}` ? TrimLeft<U> : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
